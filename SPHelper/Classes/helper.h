20a62b4ce3fbf4c77b2f4632446e5588e5beaae9
sunpeng <git@183.3.223.253:/git/mooc.git>
2019-02-16 17:41:55 +0800

    SPHelper v0.1.0

---
+++ SPHelper/Classes/helper.h
@@ -0,0 +1,19 @@
+//
+//  helper.h
+//  Test
+//
+//  Created by 孙鹏 on 2019/2/15.
+//  Copyright © 2019 孙鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface helper : NSObject
+    
+- (BOOL)isSuccess;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SPHelper/Classes/helper.m
@@ -0,0 +1,17 @@
+//
+//  helper.m
+//  Test
+//
+//  Created by 孙鹏 on 2019/2/15.
+//  Copyright © 2019 孙鹏. All rights reserved.
+//
+
+#import "helper.h"
+
+@implementation helper
+    
+- (BOOL)isSuccess {
+    return YES;
+}
+
+@end
